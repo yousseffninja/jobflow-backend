09d4986f2e5c0d4d15fb10b5994cb9f552c9cd58
yousseffninja <yousseffmohamedd22@gmail.com>
2026-07-19 05:58:24 +0300

    Initial commit

---
+++ src/main/java/com/scar/jobflow_backend/JobflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scar.jobflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scar/jobflow_backend/JobflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scar.jobflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
